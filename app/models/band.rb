6d77905bd629bafc4abc07aecf19f5fdda5c86d1
Trent B <trent.bullard@gmail.com>
2016-04-13 13:04:55 -0500

    implement band calculations

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,5 @@
 class Band < ActiveRecord::Base
+  def tolerance_pct
+    self.tolerance / 100
+  end
 end
--- app/models/resistor.rb
+++ app/models/resistor.rb
@@ -1,2 +1,40 @@
 class Resistor < ActiveRecord::Base
+
+  def CalculateOhmValue bandAColor: nil, bandBColor: nil, bandCColor: nil, bandDColor: nil
+    resistance = 0 
+    
+    if :bandAColor then 
+      band_a = Band.find_by color: bandAColor
+      resistance = band_a.significant_figures 
+      
+      if :bandBColor then 
+        band_b = Band.find_by color: bandBColor 
+        resistance = (resistance * 10) + band_b.significant_figures 
+        
+        if :bandCColor then 
+          band_c = Band.find_by color: bandCColor
+          resistance = resistance * (10 ** band_c.significant_figures)
+          
+          if :bandDColor then 
+            band_d = Band.find_by color: bandDColor
+          end
+        end
+      end
+    end
+    
+    self.resistance = resistance
+  end
+
+  def min_resistance ideal_resistance, bandDColor
+    band_d = Band.find_by color: bandDColor
+    tolerance = band_d.tolerance_pct
+    min_resistance = ideal_resistance - (ideal_resistance * band_d.tolerance_pct)
+  end
+
+  def max_resistance ideal_resistance, bandDColor
+    band_d = Band.find_by color: bandDColor
+    tolerance = band_d.tolerance_pct
+    min_resistance = ideal_resistance + (ideal_resistance * band_d.tolerance_pct)
+  end
+
 end
